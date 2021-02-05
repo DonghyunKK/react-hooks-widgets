6291b3d46eb7828d34ae0fc5fa3cf48124e0076e
donghyun kim <donghyunkim524@gmail.com>
2021-02-05 18:23:32 +0000

    learn how to add another useEffect to prevent dependency error

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,8 +2,36 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('programming');
+  const [term, setTerm] = useState('');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId)
+    };
+  }, [term]);
+
+  useEffect(() => {
+    const search = async() => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: debouncedTerm,
+        },
+      });
+
+      setResults(data.query.search);
+    };
+    search();
+  }, [debouncedTerm]);
 
   // provide second argument as a array to know when it need to execute
   // 3 options ( no second argument, empty array, array with value)
@@ -12,7 +40,7 @@ const Search = () => {
   // array with data : when the compnent is rendered for the first time and whenever data is changed
 
   // When you want to use async await in useEffect, you can't directly attach that to the fucntion.
-  useEffect(() => {
+  // useEffect(() => {
     // 1. assign helper method inside, and use with that function
     //  const search = async() => {
     //     await axios.get('url');
@@ -27,30 +55,51 @@ const Search = () => {
     // .then((response) => {
     // code.
     //});
-    const search = async() => {
-      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
-        params: {
-          action: 'query',
-          list: 'search',
-          origin: '*',
-          format: 'json',
-          srsearch: term,
-        },
-      });
+    // const search = async() => {
+    //   const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+    //     params: {
+    //       action: 'query',
+    //       list: 'search',
+    //       origin: '*',
+    //       format: 'json',
+    //       srsearch: term,
+    //     },
+    //   });
 
-      setResults(data.query.search);
-    };
-    search()
-  }, [term]);
+    //   setResults(data.query.search);
+    // };
+
+  //   if (term && !results.length) {
+  //     search();
+  //   } else {
+  //     const timeoutId = setTimeout(() => {
+  //       if (term) {
+  //         search()
+  //       }
+  //     }, 1000)
+
+  //     return () => {
+  //       clearTimeout(timeoutId);
+  //     };
+  //   }
+  // }, [term]);
 
   const renderedResults = results.map((result) => {
     return (
       <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
         <div className="content">
           <div className="header">
             {result.title}
           </div>
-          {result.snippet}
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
         </div>
       </div>
     )
