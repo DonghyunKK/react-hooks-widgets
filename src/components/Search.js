df73c85a8bf1e9902332a4bef6a948f2784c6b0e
donghyun kim <donghyunkim524@gmail.com>
2021-02-05 15:52:18 +0000

    add axios

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Search = () => {
+  const [term, setTerm] = useState('');
+
+  // provide second argument as a array to know when it need to execute
+  // 3 options ( no second argument, empty array, array with value)
+  // empty array : when the compnent is rendered for the first time only
+  // nothing : when the compnent is rendered for the first time and whenever it renders
+  // array with data : when the compnent is rendered for the first time and whenever data is changed
+  useEffect(() => {
+
+  })
+
   return (
-    <h1>Serach</h1>
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+          value={term}
+          onChange={e => setTerm(e.target.value)}
+          className="input"
+          />
+        </div>
+      </div>
+    </div>
   )
 };
 
